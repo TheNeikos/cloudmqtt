facdfce538cca68d686152a2d322d6c7abf40e05
Marcel Müller <neikos@neikos.email>
2022-07-11 18:36:17 +0200

    Add MPL header
    Signed-off-by: Marcel Müller <neikos@neikos.email>

--- mqtt-format/fuzz/fuzz_targets/packet.rs
+++ mqtt-format/fuzz/fuzz_targets/packet.rs
@@ -1,3 +1,9 @@
+//
+//   This Source Code Form is subject to the terms of the Mozilla Public
+//   License, v. 2.0. If a copy of the MPL was not distributed with this
+//   file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+
 #![no_main]
 use libfuzzer_sys::fuzz_target;
 use mqtt_format::v3::packet::mpacket;
