6c448adc2acaef9784d7d7de4a6cceb87f03cee0
Matthias Beyer <mail@beyermatthias.de>
2022-07-18 14:29:26 +0200

    Add functionality to automate sending and waiting further
    This patch adds functionality to pass commands to the
    crate::command::Command helper to send bytes and wait for specific
    bytes.
    
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- mqtt-tester/src/command.rs
+++ mqtt-tester/src/command.rs
@@ -5,6 +5,12 @@ pub struct Command {
     inner: tokio::process::Command,
 }
 
+pub enum ClientCommand {
+    Send(&'static [u8]),
+    WaitFor(&'static [u8]),
+    WaitAndCheck(Box<dyn FnOnce(&[u8]) -> bool>),
+}
+
 
 impl Command {
     pub fn new(inner: tokio::process::Command) -> Self {
@@ -12,9 +18,8 @@ impl Command {
     }
 
 
-    pub async fn wait_for_write<W, B>(mut self, writes: W) -> Result<std::process::Output, miette::Error>
-        where W: IntoIterator<Item = B>,
-              B: Iterator<Item = &'static u8>
+    pub async fn wait_for_write<C>(mut self, commands: C) -> Result<std::process::Output, miette::Error>
+        where C: IntoIterator<Item = ClientCommand>,
     {
         let mut client = self.inner
             .spawn()
@@ -23,11 +28,32 @@ impl Command {
         let mut to_client = client.stdin.take().unwrap();
         let mut from_client = client.stdout.take().unwrap();
 
-        tokio::spawn(async move { from_client.read_to_end(&mut vec![]).await });
-
-        for write in writes {
-            let buf = write.copied().collect::<Vec<u8>>();
-            to_client.write_all(&buf).await.into_diagnostic()?;
+        for command in commands {
+            match command {
+                ClientCommand::Send(bytes) => to_client.write_all(&bytes).await.into_diagnostic()?,
+                ClientCommand::WaitFor(expected_bytes) => {
+                    let mut buf = Vec::with_capacity(expected_bytes.len());
+                    match tokio::time::timeout(std::time::Duration::from_millis(100), from_client.read_exact(&mut buf)).await {
+                        Ok(Ok(_)) => if buf != expected_bytes {
+                            return Err(miette::miette!("Received Bytes did not match expected bytes: {:?} != {:?}", buf, expected_bytes))
+                        },
+                        Ok(Err(e)) => return Err(e).into_diagnostic(),
+                        Err(_elapsed) => return Err(miette::miette!("Did not hear from server until timeout")),
+                    }
+                },
+                ClientCommand::WaitAndCheck(check) => {
+                    let mut buf = Vec::new();
+                    match tokio::time::timeout(std::time::Duration::from_millis(100), from_client.read_to_end(&mut buf)).await {
+                        Ok(Ok(_)) => {
+                            if !check(&buf) {
+                                return Err(miette::miette!("Check failed for Bytes {:?}", buf))
+                            }
+                        },
+                        Ok(Err(e)) => return Err(e).into_diagnostic(),
+                        Err(_elapsed) => return Err(miette::miette!("Did not hear from server until timeout")),
+                    }
+                }
+            }
         }
 
         client.wait_with_output().await.into_diagnostic()
