9b3d11b222e9e97590ca7327465eb2f23f54f6c4
Marcel Müller <neikos@neikos.email>
2022-07-19 08:46:36 +0200

    Add test for receiving unexpected packets
    Signed-off-by: Marcel Müller <neikos@neikos.email>

--- mqtt-tester/src/client_report.rs
+++ mqtt-tester/src/client_report.rs
@@ -2,6 +2,7 @@ use std::path::{Path, PathBuf};
 use std::process::Stdio;
 use std::time::Duration;
 
+use futures::FutureExt;
 use tokio::process::Command;
 
 use crate::report::{Report, ReportResult};
@@ -23,7 +24,10 @@ pub async fn create_client_report(
 ) -> miette::Result<Vec<Report>> {
     use futures::stream::StreamExt;
 
-    let reports = vec![check_invalid_utf8_is_rejected(&client_exe_path)];
+    let reports = vec![
+        check_invalid_utf8_is_rejected(&client_exe_path).boxed_local(),
+        check_receiving_server_packet(&client_exe_path).boxed_local(),
+    ];
 
     futures::stream::iter(reports)
         .buffered(parallelism.get())
@@ -63,7 +67,7 @@ async fn check_invalid_utf8_is_rejected(client_exe_path: &Path) -> miette::Resul
             if out.status.success() {
                 ReportResult::Failure
             } else {
-                ReportResult::Failure
+                ReportResult::Success
             },
             Some(out.stderr),
         ),
@@ -76,6 +80,56 @@ async fn check_invalid_utf8_is_rejected(client_exe_path: &Path) -> miette::Resul
                                   Any receiver should immediately close the connection upon receiving such a packet."),
         normative_statement_number: String::from("[MQTT-1.5.3-1, MQTT-1.5.3-2]"),
         result,
-        output: output,
+        output,
+    })
+}
+
+async fn check_receiving_server_packet(client_exe_path: &Path) -> miette::Result<Report> {
+    let output = open_connection_with(&client_exe_path)
+        .await
+        .map(crate::command::Command::new)?
+        .wait_for_write([
+            crate::command::ClientCommand::Send(&[
+                0b0010_0000, // CONNACK
+                0b0000_0010, // Remaining length
+                0b0000_0000, // No session present
+                0b0000_0000, // Connection accepted
+            ]),
+            crate::command::ClientCommand::Send(&[
+                0b1000_0010, // SUBSCRIBE packet
+                0b0000_1000, // Length
+                // Now the variable header
+                0b0000_0000, // Packet ID
+                0b0000_0001,
+                // First sub
+                0b0000_0000,
+                0b0000_0011, // Length
+                b'a',
+                b'/',
+                b'b',
+                0b0000_0001, // QoS
+            ]),
+        ]);
+
+    let (result, output) = match tokio::time::timeout(Duration::from_millis(100), output).await {
+        Ok(Ok(out)) => (
+            if out.status.success() {
+                ReportResult::Failure
+            } else {
+                ReportResult::Success
+            },
+            Some(out.stderr),
+        ),
+        Ok(Err(_)) | Err(_) => (ReportResult::Failure, None),
+    };
+
+    Ok(Report {
+        name: String::from("Check if invalid packets are rejected"),
+        description: String::from(
+            "Unexpected packets are a protocol error and the client MUST close the connection.",
+        ),
+        normative_statement_number: String::from("[MQTT-4.8.0-1]"),
+        result,
+        output,
     })
 }
