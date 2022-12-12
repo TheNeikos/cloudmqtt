01c08161e27e7734cb77d98a8a3a2377c1d8a66c
Marcel Müller <neikos@neikos.email>
2022-12-12 12:52:48 +0100

    Add missing license header
    Signed-off-by: Marcel Müller <neikos@neikos.email>

--- src/server/state.rs
+++ src/server/state.rs
@@ -1,3 +1,8 @@
+//
+//   This Source Code Form is subject to the terms of the Mozilla Public
+//   License, v. 2.0. If a copy of the MPL was not distributed with this
+//   file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
 //! The state of a single client as defined by their ClientID
 //!
 //! # Architecture
