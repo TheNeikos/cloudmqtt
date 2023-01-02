97a32288058949f73801b219de6fe7845aa916c0
Marcel Müller <neikos@neikos.email>
2023-01-02 11:21:14 +0100

    Add serializing of Pubrel messages
    Signed-off-by: Marcel Müller <neikos@neikos.email>

--- mqtt-format/src/v3/packet.rs
+++ mqtt-format/src/v3/packet.rs
@@ -391,7 +391,20 @@ impl<'message> MPacket<'message> {
                 // Variable 1-6
                 id.write_to(&mut writer).await?;
             }
-            MPacket::Pubrel(MPubrel { id: _ }) => todo!(),
+            MPacket::Pubrel(MPubrel { id }) => {
+                let packet_type = 0b0110_0010;
+
+                // Header 1
+                writer.write_all(&[packet_type]).await?;
+
+                let remaining_length = 2;
+
+                // Header 2-5
+                write_remaining_length!(writer, remaining_length);
+
+                // Variable 1-6
+                id.write_to(&mut writer).await?;
+            }
             MPacket::Pubcomp(MPubcomp { id }) => {
                 let packet_type = 0b0111_0000;
 
