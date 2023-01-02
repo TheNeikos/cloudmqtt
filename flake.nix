e5debef8620da24dbc0458dda4fc774276539506
Marcel Müller <neikos@neikos.email>
2023-01-02 08:52:57 +0100

    Update rust to 1.66
    Signed-off-by: Marcel Müller <neikos@neikos.email>
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- flake.nix
+++ flake.nix
@@ -4,7 +4,6 @@
     nixpkgs.url = "nixpkgs/nixos-22.05";
     flake-utils = {
       url = "github:numtide/flake-utils";
-      inputs.nixpkgs.follows = "nixpkgs";
     };
     crane = {
       url = "github:ipetkov/crane";
@@ -27,7 +26,7 @@
           overlays = [ (import rust-overlay) ];
         };
 
-        rustTarget = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain;
+        rustTarget = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
         unstableRustTarget = pkgs.rust-bin.selectLatestNightlyWith (toolchain: toolchain.default.override {
           extensions = [ "rust-src" "miri" ];
         });
