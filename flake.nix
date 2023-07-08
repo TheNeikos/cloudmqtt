05b96bd2ad255052774922c0feea39c18f6877e6
Matthias Beyer <mail@beyermatthias.de>
2023-07-08 21:34:15 +0200

    Update flake input nixpkgs: 22.05 -> 23.05
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "The CloudMQTT Rust library";
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-22.05";
+    nixpkgs.url = "nixpkgs/nixos-23.05";
     flake-utils = {
       url = "github:numtide/flake-utils";
     };
