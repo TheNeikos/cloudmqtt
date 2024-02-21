2aeaeb979cbee1d862dd6eda06785c5b632b48e5
Matthias Beyer <mail@beyermatthias.de>
2024-02-21 11:46:06 +0100

    Add publish subcommand to example client
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- src/bin/cloudmqtt-client.rs
+++ src/bin/cloudmqtt-client.rs
@@ -11,7 +11,8 @@ use cloudmqtt::{
 };
 use futures::StreamExt;
 use mqtt_format::v3::{
-    strings::MString, subscription_request::MSubscriptionRequest, will::MLastWill,
+    qos::MQualityOfService, strings::MString, subscription_request::MSubscriptionRequest,
+    will::MLastWill,
 };
 use tracing::error;
 use tracing_subscriber::layer::SubscriberExt;
@@ -25,8 +26,24 @@ struct Args {
     #[clap(long, value_parser)]
     client_id: String,
 
-    #[clap(long, value_parser, required = true)]
-    subscriptions: Vec<String>,
+    #[clap(subcommand)]
+    subcommand: Subcommand,
+}
+
+#[derive(clap::Subcommand, Debug)]
+pub enum Subcommand {
+    Subscribe {
+        #[clap(long, value_parser, required = true)]
+        subscriptions: Vec<String>,
+    },
+
+    Publish {
+        #[clap(long, required = true)]
+        topic: String,
+
+        #[clap(long, required = true)]
+        message: String,
+    },
 }
 
 #[tokio::main]
@@ -71,43 +88,64 @@ async fn main() {
 
     tokio::spawn(client.heartbeat(None));
 
-    client
-        .subscribe(
-            &args
-                .subscriptions
-                .iter()
-                .map(|sub| MSubscriptionRequest {
-                    topic: MString { value: sub },
-                    qos: mqtt_format::v3::qos::MQualityOfService::ExactlyOnce,
+    match args.subcommand {
+        Subcommand::Subscribe { subscriptions } => {
+            client
+                .subscribe(
+                    &subscriptions
+                        .iter()
+                        .map(|sub| MSubscriptionRequest {
+                            topic: MString { value: sub },
+                            qos: mqtt_format::v3::qos::MQualityOfService::ExactlyOnce,
+                        })
+                        .collect::<Vec<_>>(),
+                )
+                .await
+                .unwrap();
+
+            let packet_stream = client
+                .build_packet_stream()
+                .with_custom_ack_fn(|packet| async move {
+                    println!("ACKing packet {packet:?}");
+                    Acknowledge
                 })
-                .collect::<Vec<_>>(),
-        )
-        .await
-        .unwrap();
+                .build();
+            let mut packet_stream = Box::pin(packet_stream.stream());
 
-    let packet_stream = client
-        .build_packet_stream()
-        .with_custom_ack_fn(|packet| async move {
-            println!("ACKing packet {packet:?}");
-            Acknowledge
-        })
-        .build();
-    let mut packet_stream = Box::pin(packet_stream.stream());
+            loop {
+                let packet = match packet_stream.next().await {
+                    Some(Ok(packet)) => packet,
+                    None => {
+                        error!("Stream closed unexpectedly");
+                        break;
+                    }
+                    Some(Err(error)) => {
+                        error!(?error, "Stream errored");
+                        break;
+                    }
+                };
 
-    loop {
-        let packet = match packet_stream.next().await {
-            Some(Ok(packet)) => packet,
-            None => {
-                error!("Stream closed unexpectedly");
-                break;
+                let packet = packet.get_packet();
+                println!("Received: {packet:#?}");
             }
-            Some(Err(error)) => {
-                error!(?error, "Stream errored");
-                break;
-            }
-        };
+        }
 
-        let packet = packet.get_packet();
-        println!("Received: {packet:#?}");
+        Subcommand::Publish { topic, message } => {
+            match client
+                .publish(
+                    MQualityOfService::ExactlyOnce,
+                    false,
+                    &topic,
+                    message.as_bytes(),
+                )
+                .await
+            {
+                Ok(()) => println!("Ready sending"),
+                Err(e) => {
+                    eprintln!("Sending failed: {e}");
+                    std::process::exit(1);
+                }
+            }
+        }
     }
 }
