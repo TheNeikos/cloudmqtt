d625c3fbf4a8c0e20c79c43644c3fd90bca3518c
Matthias Beyer <mail@beyermatthias.de>
2024-03-18 16:09:37 +0100

    Add parser for ReasonCode
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- mqtt-format/src/v5/mod.rs
+++ mqtt-format/src/v5/mod.rs
@@ -1,7 +1,8 @@
 pub mod bytes;
 pub mod fixed_header;
-pub mod variable_header;
 pub mod integers;
+pub mod reason_code;
 pub mod strings;
+pub mod variable_header;
 
 pub type MResult<O> = winnow::PResult<O>;
