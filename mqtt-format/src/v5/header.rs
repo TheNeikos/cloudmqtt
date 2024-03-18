f2b75d86849ed1b4cbdd27af05f2c128a5301ad0
Marcel Müller <neikos@neikos.email>
2024-03-18 14:07:56 +0100

    Add fixed header parsing
    Signed-off-by: Marcel Müller <neikos@neikos.email>

---
+++ mqtt-format/src/v5/header.rs
@@ -0,0 +1,129 @@
+use winnow::{
+    binary::bits::bits,
+    error::{ErrMode, InputError, ParserError},
+    Bytes, Parser,
+};
+
+use super::{integers::parse_variable, MResult};
+
+#[derive(Debug, PartialEq)]
+pub enum QualityOfService {
+    AtMostOnce,
+    AtLeastOnce,
+    ExactlyOnce,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum PacketType {
+    Connect,
+    Connack,
+    Publish {
+        dup: bool,
+        qos: QualityOfService,
+        retain: bool,
+    },
+    Puback,
+    Pubrec,
+    Pubrel,
+    Pubcomp,
+    Subscribe,
+    Suback,
+    Unsubscribe,
+    Unsuback,
+    Pingreq,
+    Pingresp,
+    Disconnect,
+    Auth,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct MFixedHeader {
+    packet_type: PacketType,
+    remaining_length: u32,
+}
+
+pub fn parse_fixed_header<'i>(input: &mut &'i Bytes) -> MResult<MFixedHeader> {
+    let (packet_type, packet_flags): (u8, u8) = bits::<_, _, InputError<(_, usize)>, _, _>((
+        winnow::binary::bits::take(4usize),
+        winnow::binary::bits::take(4usize),
+    ))
+    .parse_next(input)
+    .map_err(|_: ErrMode<InputError<_>>| {
+        ErrMode::from_error_kind(input, winnow::error::ErrorKind::Slice)
+    })?;
+
+    let packet_type = match (packet_type, packet_flags) {
+        (0, _) => {
+            return Err(ErrMode::from_error_kind(
+                input,
+                winnow::error::ErrorKind::Verify,
+            ))
+        }
+        (1, 0) => PacketType::Connect,
+        (2, 0) => PacketType::Connect,
+        (3, flags) => PacketType::Publish {
+            dup: (0b1000 & flags) != 0,
+            qos: match (flags & 0b0110) >> 1 {
+                0 => QualityOfService::AtMostOnce,
+                1 => QualityOfService::AtLeastOnce,
+                2 => QualityOfService::ExactlyOnce,
+                _ => {
+                    return Err(ErrMode::from_error_kind(
+                        input,
+                        winnow::error::ErrorKind::Verify,
+                    ))
+                }
+            },
+            retain: (0b0001 & flags) != 0,
+        },
+        (4, 0) => PacketType::Puback,
+        (5, 0) => PacketType::Pubrec,
+        (6, 0b0010) => PacketType::Pubrel,
+        (7, 0) => PacketType::Pubcomp,
+        (8, 0b0010) => PacketType::Subscribe,
+        (9, 0) => PacketType::Suback,
+        (10, 0b0010) => PacketType::Unsubscribe,
+        (11, 0) => PacketType::Unsuback,
+        (12, 0) => PacketType::Pingreq,
+        (13, 0) => PacketType::Pingresp,
+        (14, 0) => PacketType::Disconnect,
+        (15, 0) => PacketType::Auth,
+        _ => {
+            return Err(ErrMode::from_error_kind(
+                input,
+                winnow::error::ErrorKind::Verify,
+            ))
+        }
+    };
+
+    let remaining_length = parse_variable(input)?;
+
+    Ok(MFixedHeader {
+        packet_type,
+        remaining_length,
+    })
+}
+
+#[cfg(test)]
+mod tests {
+    use winnow::Bytes;
+
+    use crate::v5::fixed_header::{parse_fixed_header, MFixedHeader};
+
+    #[test]
+    fn check_fixed_header() {
+        let input = &[0b0011_1010, 0xA];
+
+        assert_eq!(
+            parse_fixed_header(&mut Bytes::new(&input)).unwrap(),
+            MFixedHeader {
+                packet_type: crate::v5::fixed_header::PacketType::Publish {
+                    dup: true,
+                    qos: crate::v5::fixed_header::QualityOfService::AtLeastOnce,
+                    retain: false
+                },
+                remaining_length: 10,
+            }
+        )
+    }
+}
--- mqtt-format/src/v5/mod.rs
+++ mqtt-format/src/v5/mod.rs
@@ -1,5 +1,6 @@
 mod integers;
 mod strings;
 mod bytes;
+mod header;
 
 pub type MResult<O> = winnow::PResult<O>;
