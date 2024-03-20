d5897fb0d32fc229f96d6a73458a77cd1af3f3d5
Matthias Beyer <mail@beyermatthias.de>
2024-03-20 13:58:54 +0100

    Add docs for ConnackProperties
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- mqtt-format/src/v5/packets/connack.rs
+++ mqtt-format/src/v5/packets/connack.rs
@@ -43,23 +43,58 @@ crate::v5::reason_code::make_combined_reason_code! {
 }
 
 define_properties![
+    packet_type: MConnack,
+    anker: "_Toc3901080",
     pub struct ConnackProperties<'i> {
+        (anker: "_Toc3901082")
         session_expiry_interval: SessionExpiryInterval,
+
+        (anker: "_Toc3901083")
         receive_maximum: ReceiveMaximum,
+
+        (anker: "_Toc3901084")
         maximum_qos: MaximumQoS,
+
+        (anker: "_Toc3901085")
         retain_available: RetainAvailable,
+
+        (anker: "_Toc3901086")
         maximum_packet_size: MaximumPacketSize,
+
+        (anker: "_Toc3901087")
         assigned_client_identifier: AssignedClientIdentifier<'i>,
+
+        (anker: "_Toc3901088")
         topic_alias_maximum: TopicAliasMaximum,
+
+        (anker: "_Toc3901089")
         reason_string: ReasonString<'i>,
+
+        (anker: "_Toc3901090")
         user_properties: UserProperties<'i>,
+
+        (anker: "_Toc3901091")
         wildcard_subscription_available: WildcardSubscriptionAvailable,
+
+        (anker: "_Toc3901092")
         subscription_identifiers_available: SubscriptionIdentifiersAvailable,
+
+        (anker: "_Toc3901093")
         shared_scubscription_available: SharedSubscriptionAvailable,
+
+        (anker: "_Toc3901094")
         server_keep_alive: ServerKeepAlive,
+
+        (anker: "_Toc3901095")
         response_information: ResponseInformation<'i>,
+
+        (anker: "_Toc3901096")
         server_reference: ServerReference<'i>,
+
+        (anker: "_Toc3901097")
         authentication_method: AuthenticationMethod<'i>,
+
+        (anker: "_Toc3901098")
         authentication_data: AuthenticationData<'i>,
     }
 ];
