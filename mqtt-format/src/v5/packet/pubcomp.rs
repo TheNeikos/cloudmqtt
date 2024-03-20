80a38fe9ffbdf432433968293fa12f30e31e8b24
Matthias Beyer <mail@beyermatthias.de>
2024-03-20 13:04:55 +0100

    Implement MPubcomp
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- mqtt-format/src/v5/packet/pubcomp.rs
+++ mqtt-format/src/v5/packet/pubcomp.rs
@@ -1,6 +1,39 @@
+use winnow::Bytes;
+
+use crate::v5::{
+    variable_header::{parse_packet_identifier, PacketIdentifier, ReasonString, UserProperties},
+    MResult,
+};
+
 crate::v5::reason_code::make_combined_reason_code! {
     pub enum PubcompReasonCode {
         PacketIdentifierNotFound = crate::v5::reason_code::PacketIdentifierNotFound,
         Success = crate::v5::reason_code::Success,
+    }
+}
+
+crate::v5::properties::define_properties! {
+    pub struct PubcompProperties <'i> {
+        reason_string: ReasonString<'i>,
+        user_properties: UserProperties<'i>,
+    }
+}
+
+pub struct MPubcomp<'i> {
+    packet_identifier: PacketIdentifier,
+    reason: PubcompReasonCode,
+    properties: PubcompProperties<'i>,
+}
+
+impl<'i> MPubcomp<'i> {
+    pub fn parse(input: &mut &'i Bytes) -> MResult<Self> {
+        let packet_identifier = parse_packet_identifier(input)?;
+        let reason = PubcompReasonCode::parse(input)?;
+        let properties = PubcompProperties::parse(input)?;
+        Ok(Self {
+            packet_identifier,
+            reason,
+            properties,
+        })
     }
 }
