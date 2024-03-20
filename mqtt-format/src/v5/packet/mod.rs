66ce78cf3981797489be452da215587608733627
Matthias Beyer <mail@beyermatthias.de>
2024-03-20 07:03:12 +0100

    Implement MPublish
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- mqtt-format/src/v5/packet/mod.rs
+++ mqtt-format/src/v5/packet/mod.rs
@@ -1,3 +1,4 @@
 pub mod connect;
 pub mod connack;
 pub mod payload;
+pub mod publish;
---
+++ mqtt-format/src/v5/packet/publish.rs
@@ -0,0 +1,61 @@
+use winnow::{
+    error::{ErrMode, ParserError},
+    stream::Stream,
+    Bytes,
+};
+
+use crate::v5::{
+    variable_header::{
+        ContentType, CorrelationData, MessageExpiryInterval, PayloadFormatIndicator, ResponseTopic,
+        SubscriptionIdentifier, TopicAlias, UserProperty,
+    },
+    MResult,
+};
+
+pub struct MPublish<'i> {
+    topic_name: &'i str,
+    packet_identifier: crate::v5::variable_header::PacketIdentifier,
+    properties: PublishProperties<'i>,
+    payload: &'i [u8],
+}
+
+crate::v5::properties::define_properties! {
+    pub struct PublishProperties<'i> {
+        payload_format_indicator: PayloadFormatIndicator,
+        message_expiry_interval: MessageExpiryInterval,
+        topic_alias: TopicAlias,
+        response_topic: ResponseTopic<'i>,
+        correlation_data: CorrelationData<'i>,
+        user_property: UserProperty<'i>,
+        subscription_identifier: SubscriptionIdentifier,
+        content_type: ContentType<'i>,
+    }
+}
+
+impl<'i> MPublish<'i> {
+    pub fn parse(input: &mut &'i Bytes) -> MResult<Self> {
+        let topic_name = crate::v5::strings::parse_string(input)?;
+        if !sanity_check_topic_name(&topic_name) {
+            return Err(ErrMode::from_error_kind(
+                input,
+                winnow::error::ErrorKind::Verify,
+            ));
+        }
+
+        let packet_identifier = crate::v5::variable_header::parse_packet_identifier(input)?;
+        let properties = PublishProperties::parse(input)?;
+
+        let payload = input.finish();
+
+        Ok(Self {
+            topic_name,
+            packet_identifier,
+            properties,
+            payload,
+        })
+    }
+}
+
+fn sanity_check_topic_name(topic_name: &str) -> bool {
+    topic_name.chars().all(|c| c != '#' && c != '*')
+}
