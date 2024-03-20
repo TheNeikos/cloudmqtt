d7f88c2b99b943b96c826d07afb411a28a2a0d32
Matthias Beyer <mail@beyermatthias.de>
2024-03-20 13:58:54 +0100

    Add docs for PublishProperties
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- mqtt-format/src/v5/packets/publish.rs
+++ mqtt-format/src/v5/packets/publish.rs
@@ -24,14 +24,31 @@ pub struct MPublish<'i> {
 }
 
 crate::v5::properties::define_properties! {
+    packet_type: MPublish,
+    anker: "_Toc3901109",
     pub struct PublishProperties<'i> {
+        (anker: "_Toc3901111")
         payload_format_indicator: PayloadFormatIndicator,
+
+        (anker: "_Toc3901112")
         message_expiry_interval: MessageExpiryInterval,
+
+        (anker: "_Toc3901113")
         topic_alias: TopicAlias,
+
+        (anker: "_Toc3901114")
         response_topic: ResponseTopic<'i>,
+
+        (anker: "_Toc3901115")
         correlation_data: CorrelationData<'i>,
+
+        (anker: "_Toc3901116")
         user_properties: UserProperties<'i>,
+
+        (anker: "_Toc3901117")
         subscription_identifier: SubscriptionIdentifier,
+
+        (anker: "_Toc3901118")
         content_type: ContentType<'i>,
     }
 }
