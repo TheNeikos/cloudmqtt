b4ea00c022eeeee935458acb16b0283b4bbb0412
Matthias Beyer <mail@beyermatthias.de>
2024-03-20 15:04:15 +0100

    Add missing license headers
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- mqtt-format/src/v5/packets/mod.rs
+++ mqtt-format/src/v5/packets/mod.rs
@@ -1,3 +1,9 @@
+//
+//   This Source Code Form is subject to the terms of the Mozilla Public
+//   License, v. 2.0. If a copy of the MPL was not distributed with this
+//   file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+
 use self::{
     auth::MAuth, connect::MConnect, disconnect::MDisconnect, pingreq::MPingreq,
     pingresp::MPingresp, puback::MPuback, pubcomp::MPubcomp, publish::MPublish, pubrec::MPubrec,
