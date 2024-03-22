490b1fc248dbb95542f296092bba457aa47557d7
Matthias Beyer <mail@beyermatthias.de>
2024-03-22 08:22:51 +0100

    Add test for roundtripping u32 write/parse
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- mqtt-format/src/v5/write.rs
+++ mqtt-format/src/v5/write.rs
@@ -36,3 +36,34 @@ pub trait WriteMqttPacket: Send {
         }
     }
 }
+
+#[cfg(test)]
+mod test {
+    use crate::v5::test::TestWriter;
+
+    use super::WriteMqttPacket;
+
+    #[tokio::test]
+    async fn test_write_u32_1() {
+        let mut writer = TestWriter { buffer: Vec::new() };
+        writer.write_u32(1).await.unwrap();
+        assert_eq!(writer.buffer, [0x00, 0x00, 0x00, 0x01]);
+    }
+
+    #[tokio::test]
+    async fn test_write_u32_12() {
+        let mut writer = TestWriter { buffer: Vec::new() };
+        writer.write_u32(12).await.unwrap();
+        assert_eq!(writer.buffer, [0x00, 0x00, 0x00, 12u8.to_be()]);
+    }
+
+    #[tokio::test]
+    async fn test_write_u32_range() {
+        // step by some prime number
+        for i in (0..268_435_455).step_by(271) {
+            let mut writer = TestWriter { buffer: Vec::new() };
+            writer.write_u32(i).await.unwrap();
+            assert_eq!(writer.buffer, i.to_be_bytes());
+        }
+    }
+}
