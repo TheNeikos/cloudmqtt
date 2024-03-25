65aac290470a1eb2350f4bcd67b307007f7fb544
Marcel Müller <neikos@neikos.email>
2024-03-25 09:58:13 +0100

    Extend tests for write trait
    This increases confidence in the correctness of the debug writer
    implementation
    
    Signed-off-by: Marcel Müller <neikos@neikos.email>

--- mqtt-format/src/v5/write.rs
+++ mqtt-format/src/v5/write.rs
@@ -43,31 +43,43 @@ pub trait WriteMqttPacket: Send {
 
 #[cfg(test)]
 mod test {
+    use super::WriteMqttPacket;
     use crate::v5::test::TestWriter;
 
-    use super::WriteMqttPacket;
+    #[tokio::test]
+    async fn test_write_u16() {
+        for i in (0..=u16::MAX).step_by(991) {
+            let mut writer = TestWriter { buffer: Vec::new() };
+            writer.write_u16(i).await.unwrap();
+            assert_eq!(&writer.buffer, &i.to_be_bytes());
+        }
+    }
 
     #[tokio::test]
     async fn test_write_u32_1() {
+        let num = 1;
         let mut writer = TestWriter { buffer: Vec::new() };
-        writer.write_u32(1).await.unwrap();
-        assert_eq!(writer.buffer, [0x00, 0x00, 0x00, 0x01]);
+        writer.write_u32(num).await.unwrap();
+        assert_eq!(&writer.buffer, &[0x00, 0x00, 0x00, 0x01]);
+        assert_eq!(&writer.buffer, &num.to_be_bytes());
     }
 
     #[tokio::test]
     async fn test_write_u32_12() {
+        let num = 12;
         let mut writer = TestWriter { buffer: Vec::new() };
-        writer.write_u32(12).await.unwrap();
-        assert_eq!(writer.buffer, [0x00, 0x00, 0x00, 12u8.to_be()]);
+        writer.write_u32(num).await.unwrap();
+        assert_eq!(&writer.buffer, &[0x00, 0x00, 0x00, 12u8.to_be()]);
+        assert_eq!(&writer.buffer, &num.to_be_bytes());
     }
 
     #[tokio::test]
     async fn test_write_u32_range() {
         // step by some prime number
-        for i in (0..268_435_455).step_by(271) {
+        for i in (0..268_435_455).step_by(991) {
             let mut writer = TestWriter { buffer: Vec::new() };
             writer.write_u32(i).await.unwrap();
-            assert_eq!(writer.buffer, i.to_be_bytes());
+            assert_eq!(&writer.buffer, &i.to_be_bytes());
         }
     }
 }
