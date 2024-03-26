a26caa78d74e28a501f339f872890486c67b2f11
Marcel Müller <neikos@neikos.email>
2024-03-26 08:59:16 +0100

    Add MqttPacket definition
    Signed-off-by: Marcel Müller <neikos@neikos.email>

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,4 @@
 //
 
 mod codecs;
+pub mod packet;
---
+++ src/packet.rs
@@ -0,0 +1,73 @@
+//
+//   This Source Code Form is subject to the terms of the Mozilla Public
+//   License, v. 2.0. If a copy of the MPL was not distributed with this
+//   file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+
+use std::ops::Deref;
+
+use mqtt_format::v5::packets::MqttPacket as FormatMqttPacket;
+use mqtt_format::v5::write::WriteMqttPacket;
+use stable_deref_trait::CloneStableDeref;
+use stable_deref_trait::StableDeref;
+use tokio_util::bytes::BufMut;
+use tokio_util::bytes::Bytes;
+use tokio_util::bytes::BytesMut;
+use yoke::Yoke;
+
+#[derive(Debug, Clone)]
+pub(crate) struct StableBytes(pub(crate) Bytes);
+
+impl Deref for StableBytes {
+    type Target = [u8];
+
+    fn deref(&self) -> &Self::Target {
+        self.0.deref()
+    }
+}
+
+// SAFETY: StableBytes derefs to &[u8] through bytes, which is stable
+unsafe impl StableDeref for StableBytes {}
+
+// SAFETY: StableBytes clones the pointer to the inner slice, and as such is stable as well
+unsafe impl CloneStableDeref for StableBytes {}
+
+pub struct MqttPacket {
+    pub(crate) packet: Yoke<FormatMqttPacket<'static>, StableBytes>,
+}
+
+impl MqttPacket {
+    pub fn get(&self) -> &FormatMqttPacket<'_> {
+        self.packet.get()
+    }
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum MqttWriterError {
+    #[error("An error occured while writing an MqttPacket: {:?}", .0)]
+    MqttWrite(mqtt_format::v5::write::MqttWriteError),
+}
+
+impl From<mqtt_format::v5::write::MqttWriteError> for MqttWriterError {
+    fn from(value: mqtt_format::v5::write::MqttWriteError) -> Self {
+        MqttWriterError::MqttWrite(value)
+    }
+}
+
+pub struct MqttWriter<'a>(pub &'a mut BytesMut);
+
+impl<'a> WriteMqttPacket for MqttWriter<'a> {
+    type Error = MqttWriterError;
+
+    fn write_byte(&mut self, u: u8) -> mqtt_format::v5::write::WResult<Self> {
+        self.0.put_u8(u);
+
+        Ok(())
+    }
+
+    fn write_slice(&mut self, u: &[u8]) -> mqtt_format::v5::write::WResult<Self> {
+        self.0.put_slice(u);
+
+        Ok(())
+    }
+}
