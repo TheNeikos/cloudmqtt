633f0a0f389ae8970f3a65cb5b658055ce2d64f9
Matthias Beyer <mail@beyermatthias.de>
2024-03-26 13:35:56 +0100

    Impl ClientIdentifier::new_minimal_required()
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- src/client_identifier.rs
+++ src/client_identifier.rs
@@ -11,8 +11,31 @@ pub enum ClientIdentifier {
 }
 
 impl ClientIdentifier {
-    pub fn new_minimal_required(s: impl Into<String>) -> Result<ClientIdentifier, ()> {
-        todo!()
+    pub fn new_minimal_required(
+        s: impl Into<String>,
+    ) -> Result<ClientIdentifier, ClientIdentifierError> {
+        const ALLOWED_CHARS: &str =
+            "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        let s = s.into();
+
+        let disallowed_chars = s
+            .chars()
+            .filter(|c| !ALLOWED_CHARS.chars().any(|allowed| allowed == *c))
+            .collect::<Vec<char>>();
+
+        if !disallowed_chars.is_empty() {
+            return Err(ClientIdentifierError::MinimalNotAllowedChar(
+                disallowed_chars,
+            ));
+        }
+
+        if s.len() > 23 {
+            return Err(ClientIdentifierError::MinimalTooLong(s.len()));
+        }
+
+        Ok(ClientIdentifier::MinimalRequired(
+            MinimalRequiredClientIdentifier(s),
+        ))
     }
 
     pub fn as_str(&self) -> &str {
@@ -23,3 +46,18 @@ impl ClientIdentifier {
 pub struct MinimalRequiredClientIdentifier(String);
 pub struct PotentiallyAcceptedClientIdentifier(String);
 
+#[derive(Debug, thiserror::Error)]
+pub enum ClientIdentifierError {
+    // I am ugly
+    #[error("Minimal client identifier contains disallowed characters: {}", .0.iter().copied().map(String::from).collect::<Vec<_>>().join(", "))]
+    MinimalNotAllowedChar(Vec<char>),
+
+    #[error("Minimal client identifier contains more characters than allowed: {}", .0)]
+    MinimalTooLong(usize),
+
+    #[error("Client identifier is now allowed to be of length zero")]
+    ZeroLen,
+
+    #[error("Client identifier is now allowed to be of length {}, maximum is {}", .0, u16::MAX)]
+    TooLong(usize),
+}
