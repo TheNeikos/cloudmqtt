c81ff6cbaaccfc126b6f1a87e25975570c627640
Marcel Müller <neikos@neikos.email>
2024-03-27 09:52:55 +0100

    Add SubackProperties
    Signed-off-by: Marcel Müller <neikos@neikos.email>

--- src/packets/suback.rs
+++ src/packets/suback.rs
@@ -3,3 +3,15 @@
 //   License, v. 2.0. If a copy of the MPL was not distributed with this
 //   file, You can obtain one at http://mozilla.org/MPL/2.0/.
 //
+
+crate::properties::define_properties! {
+    properties_type: mqtt_format::v5::packets::suback::SubackProperties,
+    anker: "_Toc3901174",
+    pub struct SubackProperties {
+        (anker: "_Toc3901175")
+        reason_string: ReasonString<'i> with setter = String,
+
+        (anker: "_Toc3901176")
+        user_properties: UserProperties<'i> with setter = crate::properties::UserProperty,
+    }
+}
