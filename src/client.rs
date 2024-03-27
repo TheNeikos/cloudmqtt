9487b39797586e4c39cbfd90fb5d7ac0521b080a
Matthias Beyer <mail@beyermatthias.de>
2024-03-27 09:52:43 +0100

    Move ConnectProperties to appropriate mod
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- src/client.rs
+++ src/client.rs
@@ -23,45 +23,12 @@ impl CleanStart {
     }
 }
 
-crate::properties::define_properties! {
-    properties_type: mqtt_format::v5::packets::connect::ConnectProperties,
-    anker: "_Toc3901046",
-    pub struct ConnectProperties {
-        (anker: "_Toc3901048")
-        session_expiry_interval: SessionExpiryInterval with setter = u32,
-
-        (anker: "_Toc3901049")
-        receive_maximum: ReceiveMaximum with setter = u32,
-
-        (anker: "_Toc3901050")
-        maximum_packet_size: MaximumPacketSize with setter = u32,
-
-        (anker: "_Toc3901051")
-        topic_alias_maximum: TopicAliasMaximum with setter = u32,
-
-        (anker: "_Toc3901052")
-        request_response_information: RequestResponseInformation with setter = u8,
-
-        (anker: "_Toc3901053")
-        request_problem_information: RequestProblemInformation with setter = u8,
-
-        (anker: "_Toc3901054")
-        user_properties: UserProperties<'a> with setter = crate::properties::UserProperty,
-
-        (anker: "_Toc3901055")
-        authentication_method: AuthenticationMethod<'a> with setter = String,
-
-        (anker: "_Toc3901056")
-        authentication_data: AuthenticationData<'a> with setter = Vec<u8>,
-    }
-}
-
 pub struct MqttClientConnector {
     transport: MqttConnectTransport,
     client_identifier: ClientIdentifier,
     clean_start: CleanStart,
     keep_alive: KeepAlive,
-    properties: ConnectProperties,
+    properties: crate::packets::connect::ConnectProperties,
 }
 
 impl MqttClientConnector {
@@ -76,7 +43,7 @@ impl MqttClientConnector {
             client_identifier,
             clean_start,
             keep_alive,
-            properties: ConnectProperties::new(),
+            properties: crate::packets::connect::ConnectProperties::new(),
         }
     }
 
@@ -96,7 +63,7 @@ impl MqttClientConnector {
         todo!()
     }
 
-    pub fn properties_mut(&mut self) -> &mut ConnectProperties {
+    pub fn properties_mut(&mut self) -> &mut crate::packets::connect::ConnectProperties {
         &mut self.properties
     }
 }
--- src/packets/connect.rs
+++ src/packets/connect.rs
@@ -3,3 +3,36 @@
 //   License, v. 2.0. If a copy of the MPL was not distributed with this
 //   file, You can obtain one at http://mozilla.org/MPL/2.0/.
 //
+
+crate::properties::define_properties! {
+    properties_type: mqtt_format::v5::packets::connect::ConnectProperties,
+    anker: "_Toc3901046",
+    pub struct ConnectProperties {
+        (anker: "_Toc3901048")
+        session_expiry_interval: SessionExpiryInterval with setter = u32,
+
+        (anker: "_Toc3901049")
+        receive_maximum: ReceiveMaximum with setter = u32,
+
+        (anker: "_Toc3901050")
+        maximum_packet_size: MaximumPacketSize with setter = u32,
+
+        (anker: "_Toc3901051")
+        topic_alias_maximum: TopicAliasMaximum with setter = u32,
+
+        (anker: "_Toc3901052")
+        request_response_information: RequestResponseInformation with setter = u8,
+
+        (anker: "_Toc3901053")
+        request_problem_information: RequestProblemInformation with setter = u8,
+
+        (anker: "_Toc3901054")
+        user_properties: UserProperties<'a> with setter = crate::properties::UserProperty,
+
+        (anker: "_Toc3901055")
+        authentication_method: AuthenticationMethod<'a> with setter = String,
+
+        (anker: "_Toc3901056")
+        authentication_data: AuthenticationData<'a> with setter = Vec<u8>,
+    }
+}
