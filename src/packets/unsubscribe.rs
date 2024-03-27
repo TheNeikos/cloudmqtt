404376bb70edb10b1c59a70130f6dbee45f7db22
Marcel Müller <neikos@neikos.email>
2024-03-27 09:43:38 +0100

    Add UnsubscribeProperties
    Signed-off-by: Marcel Müller <neikos@neikos.email>

--- src/packets/unsubscribe.rs
+++ src/packets/unsubscribe.rs
@@ -3,3 +3,15 @@
 //   License, v. 2.0. If a copy of the MPL was not distributed with this
 //   file, You can obtain one at http://mozilla.org/MPL/2.0/.
 //
+
+crate::properties::define_properties! {
+    properties_type: mqtt_format::v5::packets::unsubscribe::UnsubscribeProperties,
+    anker: "_Toc3901182",
+    pub struct UnsubscribeProperties {
+        (anker: "_Toc3901183")
+        subscription_identifier: SubscriptionIdentifier with setter = u32,
+
+        (anker: "_Toc3901183")
+        user_properties: UserProperties<'i> with setter = crate::properties::UserProperty,
+    }
+}
