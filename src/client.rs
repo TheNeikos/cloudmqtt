5a227a44702cd4bf4bfcf7be5d716ea13212b551
Marcel Müller <neikos@neikos.email>
2024-04-02 14:14:33 +0200

    Refactor client connection
    Signed-off-by: Marcel Müller <neikos@neikos.email>

--- src/client.rs
+++ src/client.rs
@@ -15,6 +15,7 @@ use crate::client_identifier::ProposedClientIdentifier;
 use crate::codecs::MqttPacketCodec;
 use crate::codecs::MqttPacketCodecError;
 use crate::keep_alive::KeepAlive;
+use crate::packets::connack::ConnackPropertiesView;
 use crate::string::MqttString;
 use crate::transport::MqttConnectTransport;
 use crate::transport::MqttConnection;
@@ -77,11 +78,6 @@ pub enum MqttClientConnectError {
     ServerProtocolError { reason: &'static str },
 }
 
-pub struct MqttConnected {
-    pub client: MqttClient,
-    pub properties: crate::packets::connack::ConnackPropertiesView,
-}
-
 pub struct MqttClientConnector {
     transport: MqttConnectTransport,
     client_identifier: ProposedClientIdentifier,
@@ -127,19 +123,55 @@ impl MqttClientConnector {
         self
     }
 
-    pub async fn connect(self) -> Result<MqttConnected, MqttClientConnectError> {
+    pub fn properties_mut(&mut self) -> &mut crate::packets::connect::ConnectProperties {
+        &mut self.properties
+    }
+}
+
+struct ConnectState {
+    session_present: bool,
+    receive_maximum: Option<NonZeroU16>,
+    maximum_qos: Option<mqtt_format::v5::qos::MaximumQualityOfService>,
+    retain_available: Option<bool>,
+    topic_alias_maximum: Option<u16>,
+    _conn: Framed<MqttConnection, MqttPacketCodec>,
+}
+
+struct SessionState {
+    client_identifier: MqttString,
+}
+
+pub struct MqttClient {
+    connection_state: Option<ConnectState>,
+    session_state: Option<SessionState>,
+}
+
+impl MqttClient {
+    pub fn new() -> MqttClient {
+        MqttClient {
+            connection_state: None,
+            session_state: None,
+        }
+    }
+
+    pub async fn connect(
+        &mut self,
+        connector: MqttClientConnector,
+    ) -> Result<ConnackPropertiesView, MqttClientConnectError> {
         type Mcce = MqttClientConnectError;
-        let mut conn =
-            tokio_util::codec::Framed::new(MqttConnection::from(self.transport), MqttPacketCodec);
+        let mut conn = tokio_util::codec::Framed::new(
+            MqttConnection::from(connector.transport),
+            MqttPacketCodec,
+        );
 
         let conn_packet = mqtt_format::v5::packets::connect::MConnect {
-            client_identifier: self.client_identifier.as_str(),
-            username: self.username.as_ref().map(AsRef::as_ref),
-            password: self.password.as_ref().map(AsRef::as_ref),
-            clean_start: self.clean_start.as_bool(),
-            will: self.will.as_ref().map(|w| w.as_ref()),
-            properties: self.properties.as_ref(),
-            keep_alive: self.keep_alive.as_u16(),
+            client_identifier: connector.client_identifier.as_str(),
+            username: connector.username.as_ref().map(AsRef::as_ref),
+            password: connector.password.as_ref().map(AsRef::as_ref),
+            clean_start: connector.clean_start.as_bool(),
+            will: connector.will.as_ref().map(|w| w.as_ref()),
+            properties: connector.properties.as_ref(),
+            keep_alive: connector.keep_alive.as_u16(),
         };
 
         conn.send(mqtt_format::v5::packets::MqttPacket::Connect(conn_packet))
@@ -158,7 +190,7 @@ impl MqttClientConnector {
         };
 
         let connack = loop {
-            let can_use_auth = self.properties.authentication_data.is_some();
+            let can_use_auth = connector.properties.authentication_data.is_some();
             let _auth = match maybe_connack.get() {
                 mqtt_format::v5::packets::MqttPacket::Connack(connack) => break connack,
                 mqtt_format::v5::packets::MqttPacket::Auth(auth) => {
@@ -188,18 +220,19 @@ impl MqttClientConnector {
         if connack.reason_code == mqtt_format::v5::packets::connack::ConnackReasonCode::Success {
             // TODO: Read properties, configure client
 
-            if connack.session_present && self.clean_start == CleanStart::Yes {
+            if connack.session_present && connector.clean_start == CleanStart::Yes {
                 return Err(MqttClientConnectError::ServerProtocolError {
                     reason: "MQTT-3.2.2-2",
                 });
             }
 
-            let connect_client_state = ConnectClientState {
+            let connect_client_state = ConnectState {
                 session_present: connack.session_present,
                 receive_maximum: connack.properties.receive_maximum().map(|rm| rm.0),
                 maximum_qos: connack.properties.maximum_qos().map(|mq| mq.0),
                 retain_available: connack.properties.retain_available().map(|ra| ra.0),
                 topic_alias_maximum: connack.properties.topic_alias_maximum().map(|tam| tam.0),
+                _conn: conn,
             };
 
             let assigned_client_identifier = connack.properties.assigned_client_identifier();
@@ -207,7 +240,9 @@ impl MqttClientConnector {
             let client_identifier: MqttString;
 
             if let Some(aci) = assigned_client_identifier {
-                if self.client_identifier == ProposedClientIdentifier::PotentiallyServerProvided {
+                if connector.client_identifier
+                    == ProposedClientIdentifier::PotentiallyServerProvided
+                {
                     client_identifier = MqttString::try_from(aci.0).map_err(|_mse| {
                         MqttClientConnectError::ServerProtocolError {
                             reason: "MQTT-1.5.4",
@@ -219,7 +254,7 @@ impl MqttClientConnector {
                     });
                 }
             } else {
-                client_identifier = match self.client_identifier {
+                client_identifier = match connector.client_identifier {
                     ProposedClientIdentifier::PotentiallyServerProvided => {
                         return Err(MqttClientConnectError::ServerProtocolError {
                             reason: "MQTT-3.2.2.3.7",
@@ -230,39 +265,17 @@ impl MqttClientConnector {
                 };
             }
 
-            return Ok(MqttConnected {
-                client: MqttClient {
-                    connect_client_state,
-                    client_identifier,
-                    _conn: conn,
-                },
-                properties: crate::packets::connack::ConnackPropertiesView::try_from(maybe_connack)
+            self.connection_state = Some(connect_client_state);
+            self.session_state = Some(SessionState { client_identifier });
+
+            return Ok(
+                crate::packets::connack::ConnackPropertiesView::try_from(maybe_connack)
                     .expect("An already matched value suddenly changed?"),
-            });
+            );
         }
 
         // TODO: Do something with error code
 
         todo!()
     }
-
-    pub fn properties_mut(&mut self) -> &mut crate::packets::connect::ConnectProperties {
-        &mut self.properties
-    }
 }
-
-struct ConnectClientState {
-    session_present: bool,
-    receive_maximum: Option<NonZeroU16>,
-    maximum_qos: Option<mqtt_format::v5::qos::MaximumQualityOfService>,
-    retain_available: Option<bool>,
-    topic_alias_maximum: Option<u16>,
-}
-
-pub struct MqttClient {
-    connect_client_state: ConnectClientState,
-    client_identifier: MqttString,
-    _conn: Framed<MqttConnection, MqttPacketCodec>,
-}
-
-impl MqttClient {}
