b672af4bd04d0d0a489f1db35f9b7be8f37f276d
Matthias Beyer <mail@beyermatthias.de>
2024-04-03 12:23:14 +0200

    Make QoS type PartialEq, PartialOrd
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- src/qos.rs
+++ src/qos.rs
@@ -4,7 +4,7 @@
 //   file, You can obtain one at http://mozilla.org/MPL/2.0/.
 //
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, PartialOrd)]
 pub enum QualityOfService {
     AtMostOnce,
     AtLeastOnce,
