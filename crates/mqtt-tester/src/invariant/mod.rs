d64e1b1997c35a9fb3c9672d3375afec6b48523e
Marcel Müller <neikos@neikos.email>
2025-03-18 14:34:40 +0100

    Remove mqtt-tester until further notice
    Signed-off-by: Marcel Müller <neikos@neikos.email>

--- crates/mqtt-tester/src/invariant/mod.rs
+++
@@ -1,8 +0,0 @@
-//
-//   This Source Code Form is subject to the terms of the Mozilla Public
-//   License, v. 2.0. If a copy of the MPL was not distributed with this
-//   file, You can obtain one at http://mozilla.org/MPL/2.0/.
-//
-
-pub mod connect_packet_protocol_name;
-pub mod no_username_means_no_password;
