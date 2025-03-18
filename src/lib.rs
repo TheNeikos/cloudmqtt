095f1162d95f776e1a9d3a27b3740ad1a9cd1b24
Marcel Müller <neikos@neikos.email>
2025-03-18 13:42:49 +0100

    Start rewrite by deleting all files
    Signed-off-by: Marcel Müller <neikos@neikos.email>

--- src/lib.rs
+++ src/lib.rs
@@ -3,21 +3,3 @@
 //   License, v. 2.0. If a copy of the MPL was not distributed with this
 //   file, You can obtain one at http://mozilla.org/MPL/2.0/.
 //
-
-#![deny(clippy::future_not_send)]
-
-pub mod bytes;
-pub mod client;
-pub mod client_identifier;
-mod codecs;
-mod error;
-pub mod keep_alive;
-pub mod packet_identifier;
-pub mod packets;
-pub mod payload;
-mod properties;
-pub mod qos;
-pub mod string;
-pub mod topic;
-pub mod transport;
-mod util;
