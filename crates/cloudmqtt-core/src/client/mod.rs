026c455bcd81c81cae89889eebcd4b7237bcc686
Marcel Müller <neikos@neikos.email>
2025-03-19 10:57:37 +0100

    Add Ping/keep_alive handling
    Signed-off-by: Marcel Müller <neikos@neikos.email>

--- crates/cloudmqtt-core/src/client/mod.rs
+++ crates/cloudmqtt-core/src/client/mod.rs
@@ -15,7 +15,7 @@ use rustc_hash::FxHasher;
 
 pub struct MqttClientFSM<'p> {
     data: ClientData,
-    current_state: ClientState,
+    connection_state: ConnectionState,
     to_consume_packet: Option<MqttPacket<'p>>,
 }
 
@@ -29,13 +29,20 @@ impl<'p> MqttClientFSM<'p> {
     pub fn new() -> MqttClientFSM<'p> {
         MqttClientFSM {
             data: ClientData::default(),
-            current_state: ClientState::Disconnected,
+            connection_state: ConnectionState::Disconnected,
             to_consume_packet: None,
         }
     }
 
-    pub fn handle_connect<'c>(&'_ mut self, connect: MConnect<'c>) -> ExpectedAction<'c> {
-        assert!(matches!(self.current_state, ClientState::Disconnected));
+    pub fn handle_connect<'c>(
+        &'_ mut self,
+        current_time: MqttInstant,
+        connect: MConnect<'c>,
+    ) -> ExpectedAction<'c> {
+        assert!(matches!(
+            self.connection_state,
+            ConnectionState::Disconnected
+        ));
 
         if connect.clean_start {
             // TOOD: Handle cleaning of session
@@ -62,7 +69,9 @@ impl<'p> MqttClientFSM<'p> {
             self.data.client_id_hash = client_id_hash;
         }
 
-        self.current_state = ClientState::ConnectingWithoutAuth;
+        self.connection_state = ConnectionState::ConnectingWithoutAuth(ConnectingWithoutAuth {
+            connect_sent: current_time,
+        });
 
         ExpectedAction::SendPacket(connect.into())
     }
@@ -72,27 +81,73 @@ impl<'p> MqttClientFSM<'p> {
         self.to_consume_packet = Some(message);
     }
 
-    pub fn run(&mut self) -> Option<ExpectedAction<'p>> {
-        match &self.current_state {
-            ClientState::Disconnected => {
-                panic!("Should prepare connecting before consuming messages")
+    pub fn run(&mut self, current_time: MqttInstant) -> Option<ExpectedAction<'p>> {
+        let action = match &self.connection_state {
+            ConnectionState::Disconnected => {
+                self.data.last_time_run = current_time;
+                None
             }
-            ClientState::ConnectingWithoutAuth => self.handle_connecting_without_auth(),
-            ClientState::Connected => self.handle_connected(),
-        }
+            ConnectionState::ConnectingWithoutAuth { .. } => {
+                self.handle_connecting_without_auth(current_time)
+            }
+            ConnectionState::Connected { .. } => self.handle_connected(current_time),
+        };
+
+        self.data.last_time_run = current_time;
+
+        action
     }
 
-    fn handle_connected(&mut self) -> Option<ExpectedAction<'p>> {
+    fn handle_connected(&mut self, current_time: MqttInstant) -> Option<ExpectedAction<'p>> {
+        let ConnectionState::Connected(con) = &mut self.connection_state else {
+            unreachable!()
+        };
+
+        if self.data.keep_alive > 0 {
+            match &con.ping_state {
+                PingState::WaitingForElapsed => {
+                    if con.last_time_sent.elapsed_seconds(current_time)
+                        >= self.data.keep_alive as u64
+                    {
+                        con.ping_state = PingState::WaitingForPingrespSince(current_time);
+
+                        return Some(ExpectedAction::SendPacket(MqttPacket::Pingreq(
+                            mqtt_format::v5::packets::pingreq::MPingreq,
+                        )));
+                    }
+                }
+                PingState::WaitingForPingrespSince(_mqtt_instant) => {}
+            }
+        }
+
         let incoming_packet = self.to_consume_packet.take()?;
 
         match incoming_packet {
             MqttPacket::Publish(_) => todo!(),
             MqttPacket::Disconnect(_) => todo!(),
+            MqttPacket::Pingresp(mqtt_format::v5::packets::pingresp::MPingresp) => {
+                match &con.ping_state {
+                    PingState::WaitingForPingrespSince(_since) => {
+                        con.ping_state = PingState::WaitingForElapsed;
+                        None
+                    }
+                    PingState::WaitingForElapsed => {
+                        panic!("Protocol error, got PingResp without a req");
+                    }
+                }
+            }
             _ => panic!("Invalid packet received"),
         }
     }
 
-    fn handle_connecting_without_auth(&mut self) -> Option<ExpectedAction<'p>> {
+    fn handle_connecting_without_auth(
+        &mut self,
+        _current_time: MqttInstant,
+    ) -> Option<ExpectedAction<'p>> {
+        let ConnectionState::ConnectingWithoutAuth(conn_without_auth) = &mut self.connection_state
+        else {
+            unreachable!()
+        };
         match self.to_consume_packet.take() {
             Some(MqttPacket::Connack(connack)) => {
                 if connack.reason_code != ConnackReasonCode::Success {
@@ -132,13 +187,17 @@ impl<'p> MqttClientFSM<'p> {
                     None
                 };
 
-                self.current_state = ClientState::Connected;
+                self.connection_state = ConnectionState::Connected(Connected {
+                    ping_state: PingState::WaitingForElapsed,
+                    last_time_sent: conn_without_auth.connect_sent,
+                });
+
                 potential_client_id.map(ExpectedAction::SaveClientIdentifier)
             }
-            None => match &self.current_state {
-                ClientState::Disconnected => None,
-                ClientState::ConnectingWithoutAuth => None,
-                ClientState::Connected => None,
+            None => match &self.connection_state {
+                ConnectionState::Disconnected => None,
+                ConnectionState::ConnectingWithoutAuth { .. } => None,
+                ConnectionState::Connected(Connected { .. }) => None,
             },
             p => panic!("Unexpected packet received: {p:?}"),
         }
@@ -151,36 +210,68 @@ pub enum ExpectedAction<'p> {
     SaveClientIdentifier(&'p str),
 }
 
+#[derive(Debug, Copy, Clone, Default, PartialEq, Eq, PartialOrd, Ord)]
+pub struct MqttInstant(u64);
+
+impl MqttInstant {
+    pub fn new(now: u64) -> MqttInstant {
+        MqttInstant(now)
+    }
+
+    fn elapsed_seconds(&self, current_time: MqttInstant) -> u64 {
+        self.0.abs_diff(current_time.0)
+    }
+}
+
+pub enum PingState {
+    WaitingForElapsed,
+    WaitingForPingrespSince(MqttInstant),
+}
+
 #[derive(Default)]
 pub struct ClientData {
     keep_alive: u16,
     client_id_hash: Option<u64>,
+    last_time_run: MqttInstant,
 }
 
-enum ClientState {
+struct Connected {
+    last_time_sent: MqttInstant,
+    ping_state: PingState,
+}
+
+struct ConnectingWithoutAuth {
+    connect_sent: MqttInstant,
+}
+
+enum ConnectionState {
     Disconnected,
-    ConnectingWithoutAuth,
-    Connected,
+    ConnectingWithoutAuth(ConnectingWithoutAuth),
+    Connected(Connected),
 }
 
 #[cfg(test)]
 mod tests {
     use super::MqttClientFSM;
-    use crate::client::ClientState;
+    use crate::client::ConnectionState;
+    use crate::client::ExpectedAction;
 
     #[test]
     fn check_simple_connect() {
         let mut fsm = MqttClientFSM::new();
 
-        fsm.handle_connect(mqtt_format::v5::packets::connect::MConnect {
-            client_identifier: "testing",
-            username: None,
-            password: None,
-            clean_start: false,
-            will: None,
-            properties: mqtt_format::v5::packets::connect::ConnectProperties::new(),
-            keep_alive: 0,
-        });
+        fsm.handle_connect(
+            crate::client::MqttInstant::new(0),
+            mqtt_format::v5::packets::connect::MConnect {
+                client_identifier: "testing",
+                username: None,
+                password: None,
+                clean_start: false,
+                will: None,
+                properties: mqtt_format::v5::packets::connect::ConnectProperties::new(),
+                keep_alive: 0,
+            },
+        );
 
         fsm.consume(mqtt_format::v5::packets::MqttPacket::Connack(
             mqtt_format::v5::packets::connack::MConnack {
@@ -190,9 +281,81 @@ mod tests {
             },
         ));
 
-        let action = fsm.run();
+        let action = fsm.run(crate::client::MqttInstant::new(0));
         assert!(action.is_none());
 
-        assert!(matches!(fsm.current_state, ClientState::Connected));
+        assert!(matches!(
+            fsm.connection_state,
+            ConnectionState::Connected { .. }
+        ));
+    }
+
+    #[test]
+    fn check_ping_request() {
+        let mut fsm = MqttClientFSM::new();
+
+        fsm.handle_connect(
+            crate::client::MqttInstant::new(0),
+            mqtt_format::v5::packets::connect::MConnect {
+                client_identifier: "testing",
+                username: None,
+                password: None,
+                clean_start: false,
+                will: None,
+                properties: mqtt_format::v5::packets::connect::ConnectProperties::new(),
+                keep_alive: 10,
+            },
+        );
+
+        fsm.consume(mqtt_format::v5::packets::MqttPacket::Connack(
+            mqtt_format::v5::packets::connack::MConnack {
+                session_present: false,
+                reason_code: mqtt_format::v5::packets::connack::ConnackReasonCode::Success,
+                properties: mqtt_format::v5::packets::connack::ConnackProperties::new(),
+            },
+        ));
+
+        let action = fsm.run(crate::client::MqttInstant::new(0));
+        assert!(action.is_none());
+
+        assert!(matches!(
+            fsm.connection_state,
+            ConnectionState::Connected { .. }
+        ));
+
+        let action = fsm.run(crate::client::MqttInstant::new(5));
+        assert!(action.is_none());
+
+        let action = fsm.run(crate::client::MqttInstant::new(9));
+        assert!(action.is_none());
+
+        let action = fsm.run(crate::client::MqttInstant::new(10));
+        assert!(matches!(
+            action,
+            Some(ExpectedAction::SendPacket(
+                mqtt_format::v5::packets::MqttPacket::Pingreq(..)
+            ))
+        ));
+
+        let action = fsm.run(crate::client::MqttInstant::new(12));
+        assert!(action.is_none());
+
+        fsm.consume(mqtt_format::v5::packets::MqttPacket::Pingresp(
+            mqtt_format::v5::packets::pingresp::MPingresp,
+        ));
+
+        let action = fsm.run(crate::client::MqttInstant::new(15));
+        assert!(action.is_none());
+
+        let action = fsm.run(crate::client::MqttInstant::new(20));
+        assert!(matches!(
+            action,
+            Some(ExpectedAction::SendPacket(
+                mqtt_format::v5::packets::MqttPacket::Pingreq(..)
+            ))
+        ));
+
+        let action = fsm.run(crate::client::MqttInstant::new(22));
+        assert!(action.is_none());
     }
 }
