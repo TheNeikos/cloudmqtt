ec3f125443681a05182f63c8464f8b641663f4bd
Marcel Müller <neikos@neikos.email>
2025-03-20 10:34:12 +0100

    Add publishing to MqttClient
    Signed-off-by: Marcel Müller <neikos@neikos.email>

--- crates/cloudmqtt/src/lib.rs
+++ crates/cloudmqtt/src/lib.rs
@@ -11,6 +11,7 @@ use std::time::Instant;
 use cloudmqtt_core::client::ExpectedAction;
 use cloudmqtt_core::client::MqttClientFSM;
 use cloudmqtt_core::client::MqttInstant;
+use codec::MqttPacket;
 use codec::MqttPacketCodec;
 use futures::SinkExt;
 use futures::StreamExt;
@@ -23,6 +24,7 @@ fn since(start: Instant) -> MqttInstant {
 
 pub struct CloudmqttClient {
     client_task: tokio::task::JoinHandle<()>,
+    publish_sender: tokio::sync::mpsc::Sender<MqttPacket>,
 }
 
 impl CloudmqttClient {
@@ -40,6 +42,11 @@ impl CloudmqttClient {
         let mut fsm = MqttClientFSM::new();
 
         let start = Instant::now();
+
+        let (sender, mut receiver): (
+            tokio::sync::mpsc::Sender<MqttPacket>,
+            tokio::sync::mpsc::Receiver<MqttPacket>,
+        ) = tokio::sync::mpsc::channel(1);
 
         let client_task = tokio::task::spawn(async move {
             let (reader, mut writer) = connection.split();
@@ -70,16 +77,36 @@ impl CloudmqttClient {
                 _ => unreachable!(),
             }
             loop {
-                let packet = reader
-                    .next()
-                    .await
-                    .expect("The next message to be a connack")
-                    .expect("Parsing to succeed");
+                enum GotPacket {
+                    Incoming(MqttPacket),
+                    ToPublish(MqttPacket),
+                }
+
+                let action = tokio::select! {
+                    packet = reader.next() => {
+                        if let Some(Ok(packet)) = packet {
+                            GotPacket::Incoming(packet)
+                        } else {
+                            println!("We're out, bye!");
+                            break;
+                        }
+                    }
+                    Some(packet) = receiver.recv(), if fsm.is_connected() => {
+                        GotPacket::ToPublish(packet)
+                    }
+                };
+
+                let action = match &action {
+                    GotPacket::Incoming(packet) => {
+                        fsm.consume(packet.get_packet().clone()).run(since(start))
+                    }
+                    GotPacket::ToPublish(packet) => fsm
+                        .publish(packet.get_packet().clone().try_into().unwrap())
+                        .run(since(start)),
+                };
 
                 {
-                    let fsm: &mut MqttClientFSM = &mut fsm;
-                    if let Some(action) = fsm.consume(packet.get_packet().clone()).run(since(start))
-                    {
+                    if let Some(action) = action {
                         match action {
                             ExpectedAction::SendPacket(mqtt_packet) => {
                                 writer
@@ -94,7 +121,29 @@ impl CloudmqttClient {
             }
         });
 
-        CloudmqttClient { client_task }
+        CloudmqttClient {
+            client_task,
+            publish_sender: sender,
+        }
+    }
+
+    pub async fn publish(&self, message: impl AsRef<[u8]>, topic: impl AsRef<str>) {
+        self.publish_sender
+            .send(MqttPacket::new(
+                mqtt_format::v5::packets::MqttPacket::Publish(
+                    mqtt_format::v5::packets::publish::MPublish {
+                        duplicate: false,
+                        quality_of_service: mqtt_format::v5::qos::QualityOfService::AtMostOnce,
+                        retain: false,
+                        topic_name: topic.as_ref(),
+                        packet_identifier: None,
+                        properties: mqtt_format::v5::packets::publish::PublishProperties::new(),
+                        payload: message.as_ref(),
+                    },
+                ),
+            ))
+            .await
+            .unwrap();
     }
 
     pub async fn wait_for_shutdown(&mut self) {
