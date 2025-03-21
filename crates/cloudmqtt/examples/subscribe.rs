eaaaf890924f5a3740003e12499f2c9d1eccf38f
Matthias Beyer <mail@beyermatthias.de>
2025-03-21 11:53:27 +0100

    Add subscribe example
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- crates/cloudmqtt/examples/subscribe.rs
+++ crates/cloudmqtt/examples/subscribe.rs
@@ -5,12 +5,17 @@
 //
 
 use cloudmqtt::CloudmqttClient;
+use futures::StreamExt;
 
 #[tokio::main]
 async fn main() {
     let mut client = CloudmqttClient::new("localhost:1883".to_string()).await;
 
-    client.publish(b"What's up", "foo/bar").await;
+    let mut subscription = client.subscribe("whats/up").await;
+
+    while let Some(next_message) = subscription.next().await {
+        println!("Got: {next_message:?}");
+    }
 
     client.wait_for_shutdown().await;
 }
